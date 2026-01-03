5f19f935a6af734a1ba15c41f8eb743b8b73c608
alamhubb <@012aihO>
2026-01-04 00:03:15 +0800

    支持无值属性

--- cssts/cssts-compiler/test-cssts.ts
+++
@@ -1,9 +0,0 @@
-import { transformCssTs } from 'cssts-compiler'
-
-// 模拟 Vue 文件 script 内容（有已存在的 import）
-const code = `import { computed } from 'vue'
-const a = css { displayFlex }`
-
-const result = transformCssTs(code, { styles: new Set() })
-console.log('=== OUTPUT ===')
-console.log(result.code)
