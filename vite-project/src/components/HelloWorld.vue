14f8c5690e7edd2b2043a1322539adae26a98c4a
alamhubb <alamhubb@163.com>
2026-01-14 14:00:47 +0800

    撤回修改必须使用.ts，用.vue的话 uniapp处理后会变为 路径无法渲染

--- vite-project/src/components/HelloWorld.vue
+++ vite-project/src/components/HelloWorld.vue
@@ -6,47 +6,33 @@ defineProps<{ msg: string }>()
 
 const count = ref(0)
 
-// 使用 css {} 块定义样式（支持原子类 + 自定义值）
-const cardStyle = css { 
-  padding20px
-  borderRadius8px
-  backgroundColor: '#242424'  // 自定义颜色值
-}
+// ✅ 正确语法：用逗号分隔，只使用原子类
+const cardStyle = css { padding20px, borderRadius8px }
 
 const buttonBase = css {
-  padding10px
-  padding20px  
-  borderRadius8px
-  borderNone
-  backgroundColor: '#646cff'  // 自定义颜色
-  colorWhite
-  fontSizeLarge
+  padding10px,
+  padding20px,
+  borderRadius8px,
+  borderNone,
+  colorWhite,
+  fontSizeLarge,
   cursorPointer
 }
 
-// 伪类样式使用 $$ 分隔符
-const button$$hover = css { 
-  filter: 'brightness(1.15)'
-}
-
-const button$$active = css {
-  filter: 'brightness(0.85)'
-}
+// 伪类样式
+const button$$hover = css { backgroundColorBlue }
+const button$$active = css { backgroundColorNavy }
 
 const codeStyle = css { 
-  backgroundColor: '#2d2d2d'
-  padding2px
-  padding6px
+  padding2px,
+  padding6px,
   borderRadius4px
 }
 
-const readTheDocs = css { 
-  color: '#888'
-}
+const readTheDocs = css { colorGray }
 
-const linkStyle$$hover = css { 
-  color: '#646cff'
-}
+const linkStyle = css { colorBlue }
+const linkStyle$$hover = css { colorDodgerblue }
 </script>
 
 <template>
@@ -64,13 +50,13 @@ const linkStyle$$hover = css {
 
   <p>
     Check out
-    <a :class="linkStyle$$hover" href="https://vuejs.org/guide/quick-start.html#local" target="_blank">
+    <a :class="cssts.merge(linkStyle, linkStyle$$hover)" href="https://vuejs.org/guide/quick-start.html#local" target="_blank">
       create-vue
     </a>, the official Vue + Vite starter
   </p>
   <p>
     Learn more about IDE Support for Vue in the
-    <a :class="linkStyle$$hover"
+    <a :class="cssts.merge(linkStyle, linkStyle$$hover)"
       href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support"
       target="_blank">
       Vue Docs Scaling up Guide
