9cdc5f4f62211850b6e84e7f330aff3351f9529d
alamhubb <alamhubb@163.com>
2026-01-14 22:00:12 +0800

    撤回修改必须使用.ts，用.vue的话 uniapp处理后会变为 路径无法渲染

--- vite-project/vite.config.ts
+++ vite-project/vite.config.ts
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-// 改为包名导入，让 mono 拦截
-import cssTsPlugin from 'vite-plugin-cssts'
-// import cssTsPlugin from '../vite-plugin-cssts/src/index.ts'
-import { viteMono } from 'vite-plugin-mono'   // 同样改为包名导入
+// 开发时使用相对路径，绕过 esbuild 解析问题
+import cssTsPlugin from '../vite-plugin-cssts/src/index.ts'
+import { viteMono } from 'vite-plugin-mono'
 
 // https://vite.dev/config/
 export default defineConfig({
