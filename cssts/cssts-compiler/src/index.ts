93e72255817cd702d0df05c1313151766020143c
alamhubb <alamhubb@163.com>
2026-01-18 00:52:48 +0800

    支持显示所有token

--- cssts/cssts-compiler/src/index.ts
+++ cssts/cssts-compiler/src/index.ts
@@ -61,7 +61,6 @@ export {
   generateAtomsByProperty,
   generatePropertyDts,
   generateIndexDts,
-  generateAtomPropertyMap,
   generatePseudoAtoms,
   generatePseudoDts,
   generateClassGroupAtoms,
