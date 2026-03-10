f9c2a8acd84f072339840779fa2b5d667c37923e
alamhubb <alamhubb@163.com>
2026-03-10 10:32:24 +0800

    修复容错模式问题

--- vite-project/src/TestCsstsPlugin.vue
+++ vite-project/src/TestCsstsPlugin.vue
@@ -1,40 +1,8 @@
-<script setup lang="cssts">
+<script setup lang="testts">
 import { ref } from 'vue'
 
 const count = ref(0)
-const message = 'Hello CSSTS'
-
-const cardStyle = css {
-  displayFlex,
-  flexDirectionColumn,
-  alignItemsCenter,
-  gap12px,
-  padding16px,
-  borderRadius8px,
-  borderStyleSolid,
-  borderWidth1px,
-  borderColorLightgray,
-  backgroundColorWhite
-}
-
-const countStyle = css {
-  fontSize20px,
-  fontWeight700,
-  colorBlue
-}
-
-const button$$hover = css { backgroundColorNavy }
-
-const buttonStyle = css {
-  paddingX12px,
-  paddingY8px,
-  borderNone,
-  borderRadius6px,
-  backgroundColorBlue,
-  colorWhite,
-  cursorPointer,
-  button$$hover
-}
+const message = 'Hello World'
 
 console.log(count)
 
@@ -46,9 +14,5 @@ console.log(message)
 </script>
 
 <template>
-  <div :class="cardStyle">
-    <div>{{ message }}</div>
-    <div :class="countStyle">Count: {{ count }}</div>
-    <button :class="buttonStyle" @click="increment">+1</button>
-  </div>
+  <div>{{ message }} - {{ count }}</div>
 </template>
