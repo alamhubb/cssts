36a3bf16a0f00d1bfab5249f7f78726f5e14abc1
alamhubb <alamhubb@163.com>
2026-06-28 03:02:03 +0800

    Add Qin config for CSSTS compiler

---
+++ cssts/cssts-compiler/qin.config.js
@@ -0,0 +1,33 @@
+export default {
+  name: "cssts-compiler",
+  version: "0.2.87",
+  description: "Qin-managed CSSTS parser and compiler package",
+  type: "library",
+  entry: "src/index.ts",
+  scripts: {
+    build: "npm run build",
+    test: "npm run test"
+  },
+  dependencies: {
+    "@qin/generated-qin-parser-ts": "file:../../../qin/packages/qin-language/generated/qin-parser-ts",
+    "cssts-ts": "^0.2.87",
+    "glogjs": "file:../../../glogjs",
+    "slime-ast": "^0.2.7",
+    "slime-generator": "^0.2.7",
+    "slime-parser": "^0.2.7",
+    "slime-token": "^0.2.7",
+    "subhuti": "file:../../../subhuti"
+  },
+  devDependencies: {
+    "css-tree": "^3.1.0",
+    "slime-test": "^0.2.7",
+    "tsdown": "^0.20.0-beta.3",
+    "typescript": "^5.8.3"
+  },
+  language: {
+    id: "cssts",
+    extension: ".cssts",
+    parser: "@qin/generated-qin-parser-ts",
+    compiler: "src/index.ts"
+  }
+}
